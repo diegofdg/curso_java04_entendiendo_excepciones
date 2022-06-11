ee4ba1bd93aaad3f234ae34d04f7c5bb1ca42943
diegofdg <diegofdg@gmail.com>
2022-06-11 08:14:34 -0300

    clase_04-checked_y_unchecked

--- java_stack/src/java_stack/Flujo.java
+++ java_stack/src/java_stack/Flujo.java
@@ -10,43 +10,20 @@ public class Flujo {
     }
 
     private static void metodo1() {
-        System.out.println("Inicio del metodo1");
-        metodo2();
-        System.out.println("Fin del metodo1");
+    	System.out.println("Inicio de metodo1");
+        try {
+            metodo2();
+        } catch(MiException ex) {
+            String msg = ex.getMessage();
+            System.out.println("Exception " + msg);
+            ex.printStackTrace();
+        }
+        System.out.println("Fin de metodo1");
     }
 
-    private static void metodo2() {
-        System.out.println("Inicio del metodo2");
-        ArithmeticException aException = new ArithmeticException();
-        
-        try {
-        	throw aException;			
-		} catch (Exception exception) {
-			System.out.println("Atrapo excepciones");
-			exception.printStackTrace();			
-		}        
-        
-        /*
-        for(int i = 1; i <= 5; i++) {
-        	System.out.println(i);        	
-            
-            try { // intenta esto
-            	if(i == 3) {
-            		int num = 0;
-                    int resultado = i / num;				
-                    System.out.println(resultado);            		
-            	}            	
-            	
-            	String test = null;
-            	System.out.println(test.toString());
-			} catch (ArithmeticException | NullPointerException exception) { // atrapa el error
-				System.out.println("Atrapo excepciones");
-				System.out.println(exception.getMessage());
-				exception.printStackTrace();
-			}
-        }
-        */
-        
-        System.out.println("Fin del metodo2");
-    }
+    private static void metodo2() throws MiException {
+        System.out.println("Inicio de metodo2");
+        throw new ArithmeticException("dio error");
+        // System.out.println("Fin de metodo2");
+    }	
 }
---
+++ java_stack/src/java_stack/MiException.java
@@ -0,0 +1,12 @@
+package java_stack;
+
+public class MiException extends RuntimeException {
+	
+	public MiException() {
+		super();
+	}
+	
+	public MiException(String message) {
+		super(message);
+	}
+}
